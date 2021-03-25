980a8c81cd1933835f16e4c2e4267f491e3907aa
Aryqs-Ipsum <mrfeyurikenpro@gmail.com>
2021-03-25 10:22:33 +0100

    add texteditor

--- src/components/articles/Submit.vue
+++ src/components/articles/Submit.vue
@@ -1,11 +1,21 @@
 <template>
     <div>
+        <app-header></app-header>
 
+        <main>
+            <text-editor></text-editor>
+        </main>
+
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
 export default {
-    
+    components: {
+        AppFooter: () => import('../Footer.vue'),
+        AppHeader: () => import('../Navigation.vue'),
+        TextEditor: () => import('../utils/TextEditor.vue')
+    },
 }
 </script>
--- src/components/utils/TextEditor.vue
+++ src/components/utils/TextEditor.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
+        <md-field>
+            <label>Textarea</label>
+            <md-textarea v-model="content"></md-textarea>
+        </md-field>
 
+        <md-button @click="imageUploaderOpen = true">insérer une image</md-button>
+        <image-uploader :callback="insertMarkdownImage" :open="imageUploaderOpen" :close="imageUploaderClose"></image-uploader>
     </div>
 </template>
 
 <script>
 export default {
-    
+    components: {
+        imageUploader: () => import('../utils/ImageUploader')
+    },
+    data() {
+        return {
+            imageUploaderOpen: false,
+            content: null,
+        }
+    },
+    methods: {
+        imageUploaderClose() {
+            this.imageUploaderOpen = false
+        },
+        insertMarkdownImage(url) {
+            this.content += `![remplacer ici par une légende](${url})`
+        }
+    }
 }
 </script>
