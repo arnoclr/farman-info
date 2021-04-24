13d89385bcb78063b0c3cecde3ff33f514c3b9e8
Arno Cellarier <mrfeyurikenpro@gmail.com>
2021-04-24 19:51:22 +0200

    update landing page

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -6,17 +6,32 @@
             <main>
                 
                 <div class="fm-section fm-section--full-height">
-                    <div class="fm-card fm-card--img fm-card--full-width">
-                        <div class="fm-card__img fm-card__img--left">
-                            <img src="https://i.imgur.com/pReQ6Mz.jpg" alt="Thomas Pesquet">
+                    <div class="fm-section__centered">
+                        <div class="fm-card fm-card--img fm-card--full-width">
+                            <div class="fm-card__img fm-card__img--left">
+                                <img src="https://i.imgur.com/pReQ6Mz.jpg" alt="Thomas Pesquet">
+                            </div>
+                            <div class="fm-card__body">
+                                <span class="fm-card__body-category">rediffusion</span>
+                                <h1 class="fm-card__body-title">Revivez en direct le décollage de Thomas Pesquet à bord de SpaceX.</h1>
+                                <div class="fm-card__body-content">
+                                    <p>L'astronaute français va embarquer à bord de la deuxième mission habitée de SpaceX vers l'ISS. 
+                                        Regardez l'évènement sur le YouTube de Farman.</p>
+                                    <a role="button" href="https://farman.ga/s/direct" class="fm-button">regarder</a>
+                                </div>
+                            </div>
                         </div>
-                        <div class="fm-card__body">
-                            <span class="fm-card__body-category">rediffusion</span>
-                            <h1 class="fm-card__body-title">Revivez en direct le décollage de Thomas Pesquet à bord de SpaceX.</h1>
-                            <div class="fm-card__body-content">
-                                <p>L'astronaute français va embarquer à bord de la deuxième mission habitée de SpaceX vers l'ISS. 
-                                    Regardez l'évènement sur le YouTube de Farman.</p>
-                                <a role="button" href="https://farman.ga/s/direct" class="fm-button">regarder</a>
+
+                        <div class="fm-card fm-card--img fm-card--full-width">
+                            <div class="fm-card__img fm-card__img--left">
+                                <img src="https://i.imgur.com/t6gw80c.jpg" alt="Thomas Pesquet">
+                            </div>
+                            <div class="fm-card__body">
+                                <span class="fm-card__body-category">YouTube</span>
+                                <h1 class="fm-card__body-title">Découvrez Farman sur YouTube</h1>
+                                <div class="fm-card__body-content">
+                                    <a role="button" href="https://farman.ga/s/direct" class="fm-button">regarder</a>
+                                </div>
                             </div>
                         </div>
                     </div>
