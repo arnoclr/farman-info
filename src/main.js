d55c2a9476145f1f8a8d8bcb4b5b5824d69df5cd
Arno Cellarier <mrfeyurikenpro@gmail.com>
2021-11-01 15:23:09 +0100

    update github username

--- src/main.js
+++ src/main.js
@@ -59,7 +59,7 @@ AutoAdsense()
 // console disclaimers
 console.log('%cAtttention !', 'color: #f00; background: #ff0; font-size: 24px')
 console.log('%cEn utilisant cette console, vous vous exposez au risque que des personnes malveillantes usurpent votre identité et volent vos informations par le biais d\'une attaque appelée Self-XSS.\nNe saisissez pas et ne copiez pas du code que vous ne comprenez pas.', 'font-size: 16px')
-console.log('%cLe code du site est open source → https://github.com/Aryqs-Ipsum/farman-info', 'color: #333; font-size: 16px')
+console.log('%cLe code du site est open source → https://github.com/arnoclr/farman-info', 'color: #333; font-size: 16px')
 
 let app
 
